dc19d190b5cd4eb8533e2121be48727140026e17
girumdms <164764083+girumdms@users.noreply.github.com>
2024-04-03 19:10:31 +0200

    sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,18 +4,32 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], alpha =0.6, color='green', s=75)
 
     # Create first line of best fit
+    date_df = pd.to_datetime(df['Year'], format ='%Y')
+    t = np.array(date_df)
+    t_as_numbers = mdates.date2num(t)
 
+    xs = np.array(t_as_numbers)
+    ys = np.array(df['CSIRO Adjusted Sea Level'], dtype = np.float64)
+    res = stats.linregress(xs, ys)
+    plt.plot(df['Year'], (res.slope)*(xs)+ res.intercept, 'r', label='bestfit line-1')
 
     # Create second line of best fit
-
-
+    xnew = np.arange(2000,2051,1)
+    df_xnew = pd.to_datetime(xnew, format ='%Y')
+    df_xnewn = mdates.date2num(df_xnew)
+    df_1 = np.array(df_xnewn)
+    plt.plot(xnew, (res.slope)*(df_1)+ res.intercept, 'y', label= 'bestfit line-2')    
+    
     # Add labels and title
+    plt.title('Rise in sea level', size = 20)
+    plt.xlabel('Year', size = 16)
+    plt.ylabel('Sealabel(inches)',size = 16)
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
